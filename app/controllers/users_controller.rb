b31d2281e65c583ce39373914d2af33df071a1ad
stephenvincentibanez <stephenvincentibanez@gmail.com>
2021-01-17 12:06:59 -0800

    edited user strong params to have password_confirmation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:username, :password)
+        params.require(:user).permit(:username, :password, :password_confirmation)
     end 
 
 end
