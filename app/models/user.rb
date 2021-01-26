161a8caaadd469be829dd87e665ed1b4f722d0b3
stephenvincentibanez <stephenvincentibanez@gmail.com>
2021-01-25 16:51:27 -0800

    dependent destroy on reviews for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :reviews
+    has_many :reviews, dependent: :destroy
     has_many :movies, through: :reviews
     has_many :watchlists
     has_many :moviesList, through: :watchlists, source: :movie
